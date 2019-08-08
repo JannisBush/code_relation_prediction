f67b209e7c178ac3d7cef2874209d2ef0a1ebac5
Jannis R <jannis@hash-n-bush.de>
2019-08-08 10:28:18 +0200

    Clean and comment dataset_utils

--- pytorch/run_classifier_dataset_utils.py
+++ pytorch/run_classifier_dataset_utils.py
@@ -27,7 +27,7 @@ import pandas as pd
 
 from functools import partial
 from sklearn.model_selection import train_test_split, StratifiedKFold, LeaveOneGroupOut
-from sklearn.metrics import f1_score, classification_report, confusion_matrix
+from sklearn.metrics import f1_score
 
 logger = logging.getLogger(__name__)
 
@@ -67,7 +67,7 @@ class DataProcessor(object):
     """Base class for data converters for sequence classification data sets."""
 
     def __init__(self, input_to_use):
-        """Saves the input to use."""
+        """Creates the DataProcessor and saves what to use as input."""
         self.input_to_use = input_to_use
 
     def get_train_examples(self, data_dir):
@@ -79,7 +79,7 @@ class DataProcessor(object):
         raise NotImplementedError()
 
     def get_splits(self, data_dir, splits):
-        """Gets a collection of `InputExample`s for the complete data set."""
+        """Gets a collection of `InputExample`s for the complete data set divided in splits train and dev sets."""
         raise NotImplementedError()
 
     def get_labels(self):
@@ -87,25 +87,41 @@ class DataProcessor(object):
         raise NotImplementedError()
 
     def create_inputs(self, dataset):
-        """Creates the Input Examples for a dataset."""
+        """Creates the Input Examples for a dataset depending on the input_to_use variable."""
+        # Normal mode: use the original argument as A and the response argument as B.
         if self.input_to_use == "both":
             return dataset.apply(
                 lambda x: InputExample(guid=None, text_a=x["org"], text_b=x["response"], label=x["label"]),
                 axis=1), dataset
+        # Only org mode: use the original argument as A.
         elif self.input_to_use == "org":
             return dataset.apply(
                 lambda x: InputExample(guid=None, text_a=x["org"], text_b=None, label=x["label"]),
                 axis=1), dataset
+        # Only response mode: use the response argument as A.
         elif self.input_to_use == "response":
             return dataset.apply(
                 lambda x: InputExample(guid=None, text_a=x["response"], text_b=None, label=x["label"]),
                 axis=1), dataset
+        # Reversed mode: use the response argument as A and the original argument as B.
         elif self.input_to_use == "response-org":
             return dataset.apply(
                 lambda x: InputExample(guid=None, text_a=x["response"], text_b=x["org"], label=x["label"]),
                 axis=1), dataset
+        # Invalid mode, throw an error.
         else:
             raise ValueError("Invalid input_to_use, has to be one of both, org or response.")
+
+    def _create_normal_splits(self, dataset, splits):
+        """Creates the default StratifiedKFold splits for dataset."""
+        skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=113)
+        splits_data = []
+        for train_idx, val_idx in skf.split(dataset, dataset['label']):
+            train_examples, train_df = self.create_inputs(dataset.iloc[train_idx])
+            test_examples, test_df = self.create_inputs(dataset.iloc[val_idx])
+            splits_data.append((train_examples, train_df,
+                                test_examples, test_df))
+        return splits_data
 
 
 class NoDEProcessor(DataProcessor):
@@ -122,20 +138,13 @@ class NoDEProcessor(DataProcessor):
     
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "test")
+        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "dev")
 
     def get_splits(self, data_dir, splits=2):
         """See base class."""
         df = pd.read_csv(os.path.join(data_dir, "complete_data.tsv"), sep='\t')
-        dataset = df.loc[df['org_dataset'].isin(self.train_data_names +['debate_test'])]
-        skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=113)
-        splits_data = []
-        for train_idx, val_idx in skf.split(dataset, dataset['label']):
-            train_examples, train_df = self.create_inputs(dataset.iloc[train_idx])
-            test_examples, test_df = self.create_inputs(dataset.iloc[val_idx])
-            splits_data.append((train_examples, train_df,
-                                test_examples, test_df))
-        return splits_data
+        dataset = df.loc[df['org_dataset'].isin(self.train_data_names + ['debate_test'])]
+        return self._create_normal_splits(dataset, splits)
 
     def get_labels(self):
         """See base class."""
@@ -146,10 +155,10 @@ class NoDEProcessor(DataProcessor):
         df = pd.read_csv(filename, sep='\t')
         if set_type == "train":
             dataset = df.loc[df['org_dataset'].isin(self.train_data_names)]
-        elif set_type == "test":
+        elif set_type == "dev":
             dataset = df.loc[df['org_dataset'].isin(['debate_test'])]
         else:
-            raise ValueError("Invalid set_type, has to be one of train, test or both.")
+            raise ValueError("Invalid set_type, has to be one of train or dev.")
         return self.create_inputs(dataset)
 
 
@@ -157,7 +166,7 @@ class PoliticalProcessor(DataProcessor):
     """Processor for the Political data set."""
 
     def __init__(self, task, cv_method, input_to_use):
-        """Sets the task and mode for the Political dataset."""
+        """Sets the task and mode and cv-method for the Political dataset."""
         super().__init__(input_to_use)
         self.task = task
         # Only gets used in get_splits
@@ -169,35 +178,27 @@ class PoliticalProcessor(DataProcessor):
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "test")
+        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "dev")
 
     def get_splits(self, data_dir, splits=10):
         """See base class."""
-        df = pd.read_csv(os.path.join(data_dir, "complete_data.tsv"), sep='\t')
-        df = df.loc[df['org_dataset'].isin(['political'])]
-        if self.task == "AS":
-            df = df[df['label'] != 'unrelated']
-        elif self.task == "RU":
-            df = df.replace({'label': {'attack': 'related', 'support': 'related'}})
-        dataset = df
+        df = self._get_df(os.path.join(data_dir, "complete_data.tsv"))
+        # The original CV method, stratifiedKFold.
         if self.cv_method == 'original':
-            skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=113)
-            splits_data = []
-            for train_idx, val_idx in skf.split(dataset, dataset['label']):
-                train_examples, train_df = self.create_inputs(dataset.iloc[train_idx])
-                test_examples, test_df = self.create_inputs(dataset.iloc[val_idx])
-                splits_data.append((train_examples, train_df,
-                                    test_examples, test_df))
+            splits_data = self._create_normal_splits(df, splits)
+        # Leave One Group Out CV, Groups=Topics.
         elif self.cv_method == 'topics':
             logo = LeaveOneGroupOut()
             splits_data = []
-            for train_idx, val_idx in logo.split(dataset, groups=dataset['topic']):
-                train_examples, train_df = self.create_inputs(dataset.iloc[train_idx])
-                test_examples, test_df = self.create_inputs(dataset.iloc[val_idx])
+            for train_idx, val_idx in logo.split(df, groups=df['topic']):
+                train_examples, train_df = self.create_inputs(df.iloc[train_idx])
+                test_examples, test_df = self.create_inputs(df.iloc[val_idx])
                 splits_data.append((train_examples, train_df,
                                     test_examples, test_df))
+        # Other wise invalid cv_method, throw an error.
         else:
             raise ValueError("Invalid cv_method, has to be original or topics")
+        # Return the splits
         return splits_data
 
     def get_labels(self):
@@ -213,20 +214,28 @@ class PoliticalProcessor(DataProcessor):
 
     def _create_examples(self, filename, set_type):
         """Creates examples for the training and test sets."""
-        df = pd.read_csv(filename, sep='\t')
-        df = df.loc[df['org_dataset'].isin(['political'])]
-        if self.task == "AS":
-            df = df[df['label'] != 'unrelated']
-        elif self.task == "RU":
-            df = df.replace({'label': {'attack': 'related', 'support': 'related'}})
+        df = self._get_df(filename)
+        # Train test split with 20% test size.
         train, test = train_test_split(df, test_size=0.2, random_state=113, stratify=df['label'])
         if set_type == "train":
             dataset = train
-        elif set_type == "test":
+        elif set_type == "dev":
             dataset = test
         else:
-            raise ValueError("Invalid set_type, has to be one of train, test or both.")
+            raise ValueError("Invalid set_type, has to be one of train or test.")
         return self.create_inputs(dataset)
+
+    def _get_df(self, filename):
+        """Returns the dataframe according to the task."""
+        df = pd.read_csv(filename, sep='\t')
+        df = df.loc[df['org_dataset'].isin(['political'])]
+        # Ignore the unrelated pairs in AS mode.
+        if self.task == "AS":
+            df = df[df['label'] != 'unrelated']
+        # Merge attack and support pairs in RU mode.
+        elif self.task == "RU":
+            df = df.replace({'label': {'attack': 'related', 'support': 'related'}})
+        return df
 
 
 class AgreementProcessor(DataProcessor):
@@ -235,7 +244,7 @@ class AgreementProcessor(DataProcessor):
     def __init__(self, split_topics_remove_duplicates, input_to_use):
         """Sets the mode for the Agreement dataset."""
         super().__init__(input_to_use)
-        # Only gets uses in get_train_examples and get_dev_examples
+        # Only gets uses in get_train_examples and get_dev_examples, ignored in CV.
         self.split_topics_remove_duplicates = split_topics_remove_duplicates
 
     def get_train_examples(self, data_dir):
@@ -244,20 +253,13 @@ class AgreementProcessor(DataProcessor):
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "test")
+        return self._create_examples(os.path.join(data_dir, "complete_data.tsv"), "dev")
 
     def get_splits(self, data_dir, splits=10):
         """See base class."""
         df = pd.read_csv(os.path.join(data_dir, "complete_data.tsv"), sep='\t')
         dataset = df.loc[df['org_dataset'].isin(['agreement'])]
-        skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=113)
-        splits_data = []
-        for train_idx, val_idx in skf.split(dataset, dataset['label']):
-            train_examples, train_df = self.create_inputs(dataset.iloc[train_idx])
-            test_examples, test_df = self.create_inputs(dataset.iloc[val_idx])
-            splits_data.append((train_examples, train_df,
-                                test_examples, test_df))
-        return splits_data
+        return self._create_normal_splits(dataset, splits)
 
     def get_labels(self):
         """See base class."""
@@ -267,37 +269,62 @@ class AgreementProcessor(DataProcessor):
         """Creates examples for the training and test sets."""
         df = pd.read_csv(filename, sep='\t')
         df = df.loc[df['org_dataset'].isin(['agreement'])]
+        # Random 80/20 train/test split.
         if not self.split_topics_remove_duplicates:
-            train, test = train_test_split(df, test_size=0.2, random_state=113, stratify=df['label'])
+            train, test = train_test_split(df, test_size=0.2, shuffle=True, random_state=113, stratify=df['label'])
+        # Fair mode: 80% of topics in train set, 20% of topics in test set.
         else:
-            df = df.drop_duplicates(subset=['org', 'response'])  # Removes all text duplicates
+            # Removes all text duplicates.
+            df = df.drop_duplicates(subset=['org', 'response'])
+            # Sort all topics and deterministic shuffle.
             topics = sorted(set(df['topic'].values))
-            random.Random(1).shuffle(topics)  # Deterministic shuffle
-            train = df.loc[df['topic'].isin(topics[:130])].sample(frac=1, random_state=113)  # First 80% of topics
-            test = df.loc[df['topic'].isin(topics[130:])].sample(frac=1, random_state=113)  # Last 20% of topics
-
+            random.Random(1).shuffle(topics)
+            # First 80% of topics are the train set.
+            train = df.loc[df['topic'].isin(topics[:130])].sample(frac=1, random_state=113)
+            # Last 20% of topics are the test set.
+            test = df.loc[df['topic'].isin(topics[130:])].sample(frac=1, random_state=113)
         if set_type == "train":
             dataset = train
-        elif set_type == "test":
+        elif set_type == "dev":
             dataset = test
         else:
-            raise ValueError("Invalid set_type, has to be one of train, test or both.")
+            raise ValueError("Invalid set_type, has to be one of train or dev.")
         return self.create_inputs(dataset)
 
 
 def convert_examples_to_features(examples, label_list, max_seq_length,
                                  tokenizer):
-    """Loads a data file into a list of `InputBatch`s."""
+    """Loads a list of `InputExample`s into a list of `InputFeatures`s."""
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+    # (b) For single sequences:
+    #  tokens:   [CLS] the dog is hairy . [SEP]
+    #  type_ids: 0   0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for `type=0` and
+    # `type=1` were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not *strictly* necessary
+    # since the [SEP] token unambiguously separates the sequences, but it makes
+    # it easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
 
     label_map = {label: i for i, label in enumerate(label_list)}
-
     features = []
+    # Convert all examples into features.
     for (ex_index, example) in enumerate(examples):
+        # Log info about progress all 10000 examples
         if ex_index % 10000 == 0:
             logger.info("Writing example %d of %d" % (ex_index, len(examples)))
-
+        # Tokenize text A.
         tokens_a = tokenizer.tokenize(example.text_a)
-
+        # Tokenize text B (if existent).
         tokens_b = None
         if example.text_b:
             tokens_b = tokenizer.tokenize(example.text_b)
@@ -310,52 +337,38 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
             if len(tokens_a) > max_seq_length - 2:
                 tokens_a = tokens_a[:(max_seq_length - 2)]
 
-        # The convention in BERT is:
-        # (a) For sequence pairs:
-        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-        #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
-        # (b) For single sequences:
-        #  tokens:   [CLS] the dog is hairy . [SEP]
-        #  type_ids: 0   0   0   0  0     0 0
-        #
-        # Where "type_ids" are used to indicate whether this is the first
-        # sequence or the second sequence. The embedding vectors for `type=0` and
-        # `type=1` were learned during pre-training and are added to the wordpiece
-        # embedding vector (and position vector). This is not *strictly* necessary
-        # since the [SEP] token unambiguously separates the sequences, but it makes
-        # it easier for the model to learn the concept of sequences.
-        #
-        # For classification tasks, the first vector (corresponding to [CLS]) is
-        # used as as the "sentence vector". Note that this only makes sense because
-        # the entire model is fine-tuned.
+        # Add the [CLS] and [SEP] token and the segment A(0) tokens to text A.
         tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
         segment_ids = [0] * len(tokens)
-
+        # Add the [SEP] token and the segment B(1) tokens to text B.
         if tokens_b:
             tokens += tokens_b + ["[SEP]"]
             segment_ids += [1] * (len(tokens_b) + 1)
-
+        # Convert the tokens to ids.
         input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
+        # Create a mask to only attend to real tokens.
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real tokens are attended to.
         input_mask = [1] * len(input_ids)
 
-        # Zero-pad up to the sequence length.
+        # Zero-pad up to the maximum sequence length.
         padding = [0] * (max_seq_length - len(input_ids))
         input_ids += padding
         input_mask += padding
         segment_ids += padding
 
+        # Check if all padded inputs have length equal to the maximum sequence length.
         assert len(input_ids) == max_seq_length
         assert len(input_mask) == max_seq_length
         assert len(segment_ids) == max_seq_length
 
+        # Convert the label to the corresponding label id.
         label_id = label_map[example.label]
 
+        # Log information about the first 5 examples.
         if ex_index < 5:
             logger.info("*** Example ***")
-            logger.info("guid: %s" % (example.guid))
+            logger.info("guid: %s" % example.guid)
             logger.info("tokens: %s" % " ".join(
                     [str(x) for x in tokens]))
             logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
@@ -364,11 +377,13 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
                     "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
             logger.info("label: %s (id = %d)" % (example.label, label_id))
 
+        # Create and append the input feature.
         features.append(
                 InputFeatures(input_ids=input_ids,
                               input_mask=input_mask,
                               segment_ids=segment_ids,
                               label_id=label_id))
+    # Return the list of all input features.
     return features
 
 
